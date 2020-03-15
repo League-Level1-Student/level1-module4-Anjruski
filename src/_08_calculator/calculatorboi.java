f4635f61f609ac279117cdc26f616943f4719259
Anjruski <drewpetersen@outlook.com>
2020-03-15 14:22:08 -0700

    oiasdygfkljy hsazdhfgb
    ACCORDING TO ALL KNOWN LAWS OF AVIATION, A BEE SHOULD NOT BE ABLE TO FLY. IT'S WINGS ARE TOO SMALL TO GET IT'S FAT LITTLE BODY OFF THE GROUND, YET, REGARDING THIS, IT FLIES ANYWAY

---
+++ src/_08_calculator/calculatorboi.java
@@ -0,0 +1,39 @@
+package _08_calculator;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class calculatorboi {
+public static void main(String[] args) {
+	calculatorboi cali = new calculatorboi();
+	cali.Run();
+	
+}
+	JFrame JF = new JFrame();
+	JTextField Number1 = new JTextField();
+	JTextField Number2 = new JTextField();
+	void Run() {
+		JF.setVisible(true);
+		JF.add(Number1);
+		JF.add(Number2);
+		JF.pack();
+		
+		
+	}
+	
+	void add () {
+		
+	}
+	
+	void subtract () {
+		
+	}
+	
+	void multiply () {
+		
+	}
+	
+	void divide () {
+		
+	}
+}
