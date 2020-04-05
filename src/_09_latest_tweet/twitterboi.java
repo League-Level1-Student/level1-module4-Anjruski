c639afb41a761152c9d8cb74d48b9d84682f8279
Anjruski <drewpetersen@outlook.com>
2020-04-05 14:20:42 -0700

    yeet
    corona time

---
+++ src/_09_latest_tweet/twitterboi.java
@@ -0,0 +1,40 @@
+package _09_latest_tweet;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class twitterboi implements ActionListener {
+public static void main(String[] args) {
+	twitterboi twitboi = new twitterboi();
+	twitboi.run();
+	
+}
+JFrame JF = new JFrame("twit");
+JPanel JP = new JPanel();
+JTextField JT = new JTextField(100);
+JButton JB = new JButton("search");
+void run () {
+	JF.add(JP);
+	JP.add(JT);
+	JP.add(JB);
+	JF.pack();
+	JF.setVisible(true);
+	JB.addActionListener(this);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+		
+	}
+}
+
+
+
+
+}
